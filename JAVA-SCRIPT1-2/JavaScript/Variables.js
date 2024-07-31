b872c5d81730db89af4a50b1fe97459cd23ccb89
anshul4141 <anshulprajapati691@gmail.com>
2024-07-31 13:47:19 +0530

    done

---
+++ JAVA-SCRIPT1-2/JavaScript/Variables.js
@@ -0,0 +1,14 @@
+// let, var, const
+
+//using const
+const n = 5.5;
+//n = "n";
+console.log(n);
+console.log("------------");
+// usin let
+{
+    let n = "let1st"
+    n = "let2nd"
+    console.log(n);
+}
+console.log(n);
---
+++ JAVA-SCRIPT1-2/JavaScript/ifElse.js
@@ -0,0 +1,11 @@
+const loginId = "abc";
+
+if (loginId == null || loginId != "admin") {
+
+    console.log("invalid loginid");
+
+} else {
+
+    console.log("login Successfully...");
+
+}
